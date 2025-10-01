47e8ae8157e3f546b12d2baf1a0f378e125b306a
Brian Simmons <bsimmons01@gmail.com>
2025-10-01 16:05:40 -0700

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Brian Simmons on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
